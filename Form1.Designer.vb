24f0609977c2f5719256a6162c8720c58c9d33f3
Edgar Eduardo Vega Ruiz <edgarevegar04@gmail.com>
2023-05-17 13:26:26 -0700

    Mi primer commit -E

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,16 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(284, 53)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(646, 308)
+        ClientSize = New Size(738, 411)
+        Controls.Add(Button1)
+        Margin = New Padding(3, 4, 3, 4)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
     End Sub
 
+    Friend WithEvents Button1 As Button
 End Class
